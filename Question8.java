4765dd7697a089b929704d6cfb42cdbf0c92ea9c
rajdeep-dabral <mail.rajdeepdabral@gmail.com>
2021-02-21 17:21:27 +0530

    Question done

--- Question8.java
+++ Question8.java
@@ -4,27 +4,25 @@ Q8. Implement multiple inheritance with default method inside interface.
  */
 
 interface GrandParent{
-    default void grandParentDefaultMethod(){
+    default void defaultMethod(){
         System.out.println("Hi i am grandParent Default Method!!!");
     }
 }
-interface Parent extends GrandParent{
-    default void ParentDefaultMethod(){
+interface Parent{
+    default void defaultMethod(){
         System.out.println("Hi i am Parent Default method!!!");
     }
 }
-interface Child extends Parent{
-    default void childDefaultMethod(){
+interface Child extends Parent , GrandParent{
+    default void defaultMethod(){
         System.out.println("Hi I am child Default method!!!");
     }
 }
 
-public class ProgramQuestions implements Child{
+public class Program implements Child{
     public static void main(String [] args){
-        Child c = new ProgramQuestions();
-        c.grandParentDefaultMethod();
-        c.ParentDefaultMethod();
-        c.childDefaultMethod();
+        Child c = new Program();
+        c.defaultMethod();
     }
 }
 
