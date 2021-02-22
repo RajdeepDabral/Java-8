fba8edb1407a5b2ce569fef72d2e0f917521064d
RajdeepDabral <62365960+RajdeepDabral@users.noreply.github.com>
2021-02-22 13:59:41 +0530

    Update Question11.java

--- Question11.java
+++ Question11.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
 public class ProgramQuestions{
     public static void main(String [] args){
         IntStream list = IntStream.of(1,2,3,4,5,6,7,8,9,10);
-        OptionalDouble obj = list.map((x) -> x * x).average();
+        OptionalDouble obj = list.map((x) -> x + x).average();
         System.out.println(obj);
     }
 }
